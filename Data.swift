e76f6a79a473d30b014bab7f5f46fa6ccf783e92
Matt Blanc <mathewpblanc@gmail.com>
2025-02-24 12:12:31 +0100

    Added data structures and examples

---
+++ Data.swift
@@ -0,0 +1,71 @@
+//
+//  Data.swift
+//  WeCanSave
+//
+//  Created by Mathew Blanc on 24/02/25.
+//
+
+import Foundation
+import SwiftUI
+import SwiftData
+import MapKit
+
+struct User: Identifiable {
+    var id = UUID()
+    var trips: [Trip]
+}
+
+struct Trip: Identifiable {
+    var id = UUID()
+//    let destinationID: UUID
+    let destinationName: String
+    let destinationLatLong: CLLocationCoordinate2D
+    let destinationLat: String
+    let destinationLong: String
+    var startDate: Date
+    var endDate: Date
+    var category: String
+    var bag: Bag
+    
+    static let exampleTrip = Trip(
+        destinationName: "Dublin",
+        destinationLatLong: CLLocationCoordinate2D(latitude: 53.34570, longitude: -6.268386),
+        destinationLat: "53.34570",
+        destinationLong: "-6.268386",
+        startDate: Calendar.current.date(from: DateComponents(year: 2025, month: 5, day: 12))!,
+        endDate: Calendar.current.date(from: DateComponents(year: 2025, month: 5, day: 18))!,
+        category: "Adventure",
+        bag: Bag.exampleBag
+    )
+}
+
+struct Bag: Identifiable {
+    var id = UUID()
+    var itemList = [BagItem]()
+    
+    static let exampleBag = Bag(itemList: [
+        BagItem.socks,
+        BagItem.tops,
+        BagItem.shoes,
+        BagItem.charger,
+    ])
+}
+
+struct BagItem: Identifiable {
+    var id = UUID()
+    let name: String
+    let category: String
+    var quantity: Int
+    var imageName: String?
+    var isPair = false
+    
+    static let socks = BagItem(name: "Socks", category: "Clothes", quantity: 4, isPair: true)
+    static let tops = BagItem(name: "Tops", category: "Clothes", quantity: 6)
+    static let shoes = BagItem(name: "Shoes", category: "Shoes", quantity: 2, isPair: true)
+    static let charger = BagItem(name: "Charger", category: "Electronics", quantity: 1)
+}
+
+
+
+
+
