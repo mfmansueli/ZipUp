04502a12fb3ec44b1550d9294922070f33316e28
CinziaFerrara <cinzia.ferrara.main@gmail.com>
2025-03-12 09:49:34 +0100

    implemeted the empty StateView

---
+++ WeCanSave/View/TripsList/TripsListEmptyView.swift
@@ -0,0 +1,16 @@
+  Group {
+                        Text("Where")
+                            .foregroundColor(.brandOrange) +
+                        Text(" are you going next?")
+                    }
+                    .font(.system(size: 45, weight: .bold))
+                    //.padding(.top, -200) // Corretto il padding
+                    .multilineTextAlignment(.center)// Group {
+                    
+                    Text("\nCreate your first trip to get started.")
+                        //.padding(-130)
+                    
+                    Image("PlanePath")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 20)
